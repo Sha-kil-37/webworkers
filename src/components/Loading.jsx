5ff3429a127f6d2be28003b7214b837f5cadcae8
Shakil <sakildevmern@gmail.com>
2025-11-22 21:30:43 +0600

    11/22/2025

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function Loading() {
   return (
-    <section className="h-full w-full overflow-hidden flex justify-center item-center">
-      <h1 className="font-3xl font-bold ">Please Wait....</h1>
+    <section className="flex justify-center items-center h-screen">
+      <h1 className="text-xl font-bold ">Please Wait....</h1>
     </section>
   );
 }
