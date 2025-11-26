158cf8ca3aaac65260db2ae4c2c413a88dd96d02
Shakil <sakildevmern@gmail.com>
2025-11-26 20:14:26 +0600

    11/26/2025

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 //
 export default function Services() {
-  return <div>Services</div>;
+  return (
+    <section>
+      <div className="max-w-7xl mx-auto">
+        <h2 className="font-bold text-4xl">Our Services</h2>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus suscipit exercitationem quod nisi odit atque magnam nesciunt doloribus expedita, ipsam, animi necessitatibus voluptas consequuntur eos minus. Dignissimos debitis vero corrupti?</p>
+      </div>
+    </section>
+  );
 }
