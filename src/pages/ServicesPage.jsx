72430729298df08242397518af22d775eebe24ad
Shakil <sakildevmern@gmail.com>
2025-11-26 20:17:36 +0600

    11/26/2025

--- src/pages/ServicesPage.jsx
+++ src/pages/ServicesPage.jsx
@@ -12,28 +12,31 @@ const MainLayout = lazy(() => import("../layout/MainLayout"));
 export default function ServicesPage() {
   //
   const questions = [
-  {
-    id: 1,
-    question: 'Popular Articles',
-    answer: 'Suspendisse ipsum elit, hendrerit id eleifend at, condimentum et mauris. Curabitur et libero vel arcu dignissim pulvinar ut ac leo. In sit amet orci et erat accumsan interdum.',
-  },
     {
-    id: 2,
-    question: 'Fix problems & request removals',
-    answer: 'Suspendisse ipsum elit, hendrerit id eleifend at, condimentum et mauris. Curabitur et libero vel arcu dignissim pulvinar ut ac leo. In sit amet orci et erat accumsan interdum.',
-  },
+      id: 1,
+      question: "Popular Articles",
+      answer:
+        "Suspendisse ipsum elit, hendrerit id eleifend at, condimentum et mauris. Curabitur et libero vel arcu dignissim pulvinar ut ac leo. In sit amet orci et erat accumsan interdum.",
+    },
     {
-    id: 3,
-    question: 'Browse the web',
-    answer: 'Suspendisse ipsum elit, hendrerit id eleifend at, condimentum et mauris. Curabitur et libero vel arcu dignissim pulvinar ut ac leo. In sit amet orci et erat accumsan interdum.',
-  },
-      {
-    id: 4,
-    question: 'Search on your phone or tablet',
-    answer: 'Suspendisse ipsum elit, hendrerit id eleifend at, condimentum et mauris. Curabitur et libero vel arcu dignissim pulvinar ut ac leo. In sit amet orci et erat accumsan interdum.',
-  },
-  
-]
+      id: 2,
+      question: "Fix problems & request removals",
+      answer:
+        "Suspendisse ipsum elit, hendrerit id eleifend at, condimentum et mauris. Curabitur et libero vel arcu dignissim pulvinar ut ac leo. In sit amet orci et erat accumsan interdum.",
+    },
+    {
+      id: 3,
+      question: "Browse the web",
+      answer:
+        "Suspendisse ipsum elit, hendrerit id eleifend at, condimentum et mauris. Curabitur et libero vel arcu dignissim pulvinar ut ac leo. In sit amet orci et erat accumsan interdum.",
+    },
+    {
+      id: 4,
+      question: "Search on your phone or tablet",
+      answer:
+        "Suspendisse ipsum elit, hendrerit id eleifend at, condimentum et mauris. Curabitur et libero vel arcu dignissim pulvinar ut ac leo. In sit amet orci et erat accumsan interdum.",
+    },
+  ];
 
   //
 
@@ -95,7 +98,7 @@ export default function ServicesPage() {
           </Paragraph>
           <OurValue />
           <Services />
-          <FAQ data={questions} />
+          {/* <FAQ data={questions} /> */}
         </SmoothScroll>
       </MainLayout>
     </Suspense>
