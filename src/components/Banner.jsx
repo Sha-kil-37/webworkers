a0fd57d484f79bb9b96f16c9cddf304c3439ac01
Shakil <sakildevmern@gmail.com>
2025-12-23 18:02:17 +0600

    12/23/2025

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,5 +1,6 @@
 import Marquee from "react-fast-marquee";
 import { useEffect, useRef } from "react";
+import Paragraph from "./Paragraph";
 //
 // Banner component with scroll-driven zoom effect
 export default function Banner() {
@@ -12,7 +13,7 @@ export default function Banner() {
     if (!el) return;
 
     let rafId = null;
-
+    //
     const onScroll = () => {
       if (rafId) cancelAnimationFrame(rafId);
       rafId = requestAnimationFrame(() => {
@@ -47,16 +48,26 @@ export default function Banner() {
   return (
     <section
       ref={bannerRef}
-      className="relative py-40 bg-amber-200 overflow-hidden"
+      className="relative py-25 bg-amber-200 overflow-hidden"
     >
       <div
         ref={frameRef}
         className="max-w-6xl mx-auto relative z-10 text-center transform-gpu"
         style={{ willChange: "transform" }}
       >
-        <h1 className="transition-colors duration-300 font-sens font-bold text-7xl mt-3 max-w-2xl mx-auto ">
+        <h1 className="transition-colors duration-300 font-sens font-bold text-7xl mt-3 max-w-2xl mx-auto">
           We Build Creative Digital Solutions
         </h1>
+        <Paragraph className="mt-10 max-w-4xl mx-auto">
+          In today’s fast-moving digital world, Every business needs a strong
+          online presence to thrive in the digital world. With{" "}
+          <em className="font-bold text-red-300">
+            “We Build Creative Digital Solutions,”
+          </em>{" "}
+          we deliver innovative, user-friendly Web Development, Digital
+          Marketing, and UI/UX Design that help businesses attract customers,
+          strengthen their brand, and grow online
+        </Paragraph>
         <button
           type="button"
           className="mt-10 block py-2 rounded-full dark:bg-white dark:text-black bg-black w-80 mx-auto font-bold text-white cursor-pointer"
