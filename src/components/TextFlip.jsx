edde08894808243bf93d060748651912d0995c0b
Shakil <sakildevmern@gmail.com>
2026-01-11 23:51:50 +0600

    01/11/2026

--- src/components/TextFlip.jsx
+++ src/components/TextFlip.jsx
@@ -17,13 +17,13 @@ export const TextFlip = ({ text, words, duration }) => {
     <>
       <motion.span
         layoutId="subtext"
-        className="2xl:text-5xl xl:text-5xl 2xl:font-bold xl:font-bold xl:tracking-wide 2xl:tracking-wide font-primary block text-[#0076DF] 2xl:w-xl xl:w-xl lg:text-5xl lg:font-bold lg:tracking-wide lg:w-lg md:text-5xl md:tracking-wide md:font-bold md:w-lg sm:w-lg sm:text-4xl sm:tracking-wide sm:font-bold font-bold text-xl tracking-wide"
+        className="2xl:text-5xl xl:text-5xl 2xl:font-bold xl:font-bold xl:tracking-wide 2xl:tracking-wide font-primary block text-[#0076DF] 2xl:w-xl xl:w-xl lg:text-5xl lg:font-bold lg:tracking-wide lg:w-lg md:text-5xl md:tracking-wide md:font-bold md:w-lg sm:w-lg sm:text-4xl sm:tracking-wide sm:font-bold font-bold text-2xl tracking-wide"
       >
         {text}
       </motion.span>
       <motion.span
         layout
-        className="xl:mt-5 lg:mt-5 md:mt-5 xl:text-5xl xl:font-bold xl:tracking-wide font-primary block lg:text-5xl lg:font-bold lg:tracking-wide md:text-5xl md:tracking-wide md:font-bold sm:text-3xl sm:tracking-wide sm:font-bold sm:w-full font-bold tracking-wide mt-5 text-xl"
+        className="xl:mt-5 lg:mt-5 md:mt-5 xl:text-5xl xl:font-bold xl:tracking-wide font-primary block lg:text-5xl lg:font-bold lg:tracking-wide md:text-5xl md:tracking-wide md:font-bold sm:text-3xl sm:tracking-wide sm:font-bold sm:w-full font-bold tracking-wide mt-5 text-2xl"
       >
         <AnimatePresence mode="popLayout">
           <motion.span
